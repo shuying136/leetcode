477029997ca18d7529c2777ac216cea8c1e8330d
shuying136 <shuying1369@163.com>
2017-03-23 10:52:05 +0800

    Create bianli.cpp

---
+++ bianli.cpp
@@ -0,0 +1,19 @@
+int row=matrix.size(),col=matrix[0].size();
+        vector<vector<int> >dp(row,vector<int>(col,INT_MAX));
+        for(int times=0;times<=1;times++)
+            for(int i=times?row-1:0;times?i>=0:i<row;times?i--:i++)
+                for(int j=times?col-1:0;times?j>=0:j<col;times?j--:j++)
+                    if(!matrix[i][j]){
+                        dp[i][j]=0;
+                    }
+                    else if(dp[i][j]){
+                        if(i&&dp[i-1][j]!=INT_MAX&&dp[i][j]>dp[i-1][j]+1)
+                            dp[i][j]=dp[i-1][j]+1;
+                        if(j+1<col&&dp[i][j+1]!=INT_MAX&&dp[i][j]>dp[i][j+1]+1)
+                            dp[i][j]=dp[i][j+1]+1;
+                        if(i+1<row&&dp[i+1][j]!=INT_MAX&&dp[i][j]>dp[i+1][j]+1)
+                            dp[i][j]=dp[i+1][j]+1;
+                        if(j&&dp[i][j-1]!=INT_MAX&&dp[i][j]>dp[i][j-1]+1)
+                            dp[i][j]=dp[i][j-1]+1;
+                    }
+        return dp;
